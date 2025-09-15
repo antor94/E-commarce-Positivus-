ddc53254065bb21e2e352f39746fda48bb774cfe
Antor Web-Dev <antorremeni@gmail.com>
2025-09-15 19:29:46 +0600

    change

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CommonHead from './common/CommonHead'
+import conImg from '../assets/images/contctImg.png'
 
 const Contactus = () => {
   return (
@@ -13,9 +14,27 @@ const Contactus = () => {
             <CommonHead commonh2={'Contact Us'} />
             <div className='w-[330px]'><CommonHead commonp={'Connect with Us: Lets Discuss Your Digital Marketing Needs'} /></div>
             </div>
-            <div id='contact-id'>
+            <div id='contact-id' className='bg-[#F3F3F3] rounded-[45px]'>
 
-            </div>
+                {/* -------------- left-side */}
+
+                <div>
+                    <div>
+                        <div>
+                            <input id='con' type="radio" />
+                            <label htmlFor="con">Say Hi</label>
+                        </div>
+                          <div>
+                            <input id='con' type="radio" />
+                            <label htmlFor="con">Get a Quote</label>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+           </div>
         </div>
     </section>
     
