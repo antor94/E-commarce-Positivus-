b546c7a71248031cb6917d3b104dd0ecd3c5f188
Antor Web-Dev <antorremeni@gmail.com>
2025-09-15 19:15:00 +0600

    chnage

--- src/components/Bannerlogo.jsx
+++ src/components/Bannerlogo.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'react-router'
 
 import comLogo from '../assets/images/Company logo.png'
-import comLogo from '../assets/images/Company logo1.png'
-import comLogo from '../assets/images/Company logo2.png'
-import comLogo from '../assets/images/Company logo3.png'
-import comLogo from '../assets/images/Company logo4.png'
-import comLogo from '../assets/images/Company logo.png'
-import comLogo from '../assets/images/Company logo.png'
+import comLogo1 from '../assets/images/Company logo1.png'
+import comLogo2 from '../assets/images/Company logo2.png'
+import comLogo3 from '../assets/images/Company logo3.png'
+import comLogo4 from '../assets/images/Company logo4.png'
+import comLogo5 from '../assets/images/Company logo5.png'
+
 
 
 const Bannerlogo = () => {
@@ -16,9 +16,20 @@ const Bannerlogo = () => {
     
     
     
-    <section id='com-logo'>
+    <section id='com-logo' className='pb-[140px]'>
       <div className="container">
         <div id='com-logo-id'>
+
+          <div>
+            <ul className='flex justify-center items-center gap-[96px]'>
+              <li><Link to={'/'} ><img src={comLogo} alt="com-logo" /></Link></li>
+              <li><Link to={'/'} ><img src={comLogo1} alt="com-logo" /></Link></li>
+              <li><Link to={'/'} ><img src={comLogo2} alt="com-logo" /></Link></li>
+              <li><Link to={'/'} ><img src={comLogo3} alt="com-logo" /></Link></li>
+              <li><Link to={'/'} ><img src={comLogo4} alt="com-logo" /></Link></li>
+              <li><Link to={'/'} ><img src={comLogo5} alt="com-logo" /></Link></li>
+            </ul>
+          </div>
 
 
 
