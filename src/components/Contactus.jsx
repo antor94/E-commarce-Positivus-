456df5d65c1c67b8e14a371b71f1c25ee7a90438
Antor Web-Dev <antorremeni@gmail.com>
2025-09-16 10:22:02 +0600

    chnage

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -9,12 +9,11 @@ const Contactus = () => {
     <section id='contact' className=''>
         <div className="container">
             {/* ------------ commonHead */}
-
-            <div className='flex gap-[40px]'>
+            <div className='flex pb-[80px] gap-[40px]'>
             <CommonHead commonh2={'Contact Us'} />
             <div className='w-[330px]'><CommonHead commonp={'Connect with Us: Lets Discuss Your Digital Marketing Needs'} /></div>
             </div>
-            <div id='contact-row' className='bg-[#F3F3F3] pb-[80px] rounded-[45px]'>
+            <div id='contact-row' className='bg-[#F3F3F3] pb-[80px] rounded-[45px] flex  relative  '>
 
                 {/* -------------- left-side */}
 
@@ -27,8 +26,8 @@ const Contactus = () => {
                             <label className='text-[16px] font-normal font-main text-black' htmlFor="con">Say Hi</label>
                         </div>
                           <div className='flex gap-[14px]'>
-                            <input id='con' type="radio" />
-                            <label className='text-[16px] font-normal font-main text-black' htmlFor="con">Get a Quote</label>
+                            <input id='contact' type="radio" />
+                            <label className='text-[16px] font-normal font-main text-black' htmlFor="contact">Get a Quote</label>
                         </div>
                     </div>
 
@@ -48,7 +47,7 @@ const Contactus = () => {
                         
                         <p className='text-[16px] font-normal font-main  text-black'>Email*</p>
                         <div className='w-[556px] mt-[5px] border rounded-[14px]'>
-                        <input className='py-[18px] bg-white pl-[30px] rounded-[14px] text-[18px] font-normal font-main text-[#898989] w-full border-none outline-none' type="text" placeholder='Email' />
+                        <input className='py-[18px] bg-white pl-[30px] rounded-[14px] text-[18px] font-normal font-main text-[#898989] w-full border-none outline-none' type="email" placeholder='Email' />
                         </div>
 
                         </div>
@@ -77,6 +76,9 @@ const Contactus = () => {
 
 
                 </div>
+
+                {/* ------------- right-img */}
+                <div className=''><img src={conImg} alt="contact_img" /></div>
 
 
 
