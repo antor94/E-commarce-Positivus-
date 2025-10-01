e50972b0cca8e2b2099e619d8477959eae86066c
Antor Web-Dev <antorremeni@gmail.com>
2025-10-01 17:17:01 +0600

    chnage

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -21,7 +21,7 @@ const Contactus = () => {
 
                 <div>
 
-                    {/* -------------- buuton */}
+                    {/* -------------- button */}
                     <div className='flex gap-[35px]'>
                         <div className='flex gap-[14px]'> 
                             <input id='con' type="radio" />
@@ -41,20 +41,12 @@ const Contactus = () => {
                         <div className='w-[556px] mt-[5px] border rounded-[14px]'>
                         <input className='py-[18px] bg-white pl-[30px] rounded-[14px] text-[18px] font-normal font-main text-[#898989] w-full border-none outline-none' type="text" placeholder='Name' />
                         </div>
-
-
-                        <div className='my-[25px]'>
-
-
-                        
+                        <div className='my-[25px]'>                       
                         <p className='text-[16px] font-normal font-main  text-black'>Email*</p>
                         <div className='w-[556px] mt-[5px] border rounded-[14px]'>
                         <input className='py-[18px] bg-white pl-[30px] rounded-[14px] text-[18px] font-normal font-main text-[#898989] w-full border-none outline-none' type="email" placeholder='Email' />
                         </div>
-
                         </div>
-
-
                         <p className='text-[16px] font-normal font-main  text-black'>Message*</p>
                         <textarea className='w-[556px] border rounded-[14px] text-[18px] font-normal font-main text-[#898989]' placeholder='Message'></textarea>
                        
@@ -62,21 +54,6 @@ const Contactus = () => {
                        {/* ------------ button */}
 
                        <button className='w-[556px] mt-[40px] bg-[#191A23] text-[20px] font-normal font-main text-[#fff] py-[20px] rounded-[14px]'>Send Message</button>
-
-
-                        
-                    
-
-
-
-
-
-
-
-
-
-
-
                 </div>
 
                 {/* ------------- right-img */}
