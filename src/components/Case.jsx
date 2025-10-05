482a7d616539e9c835c03c28620b53cdc3abf683
Antor Web-Dev <antorremeni@gmail.com>
2025-10-05 09:55:55 +0600

    chnagre

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -14,13 +14,27 @@ const Case = () => {
             <div id='case-id' className='bg-[#191A23]'>
 
 
-<div className='w-[286px] h-[163px]'>
+<div  className='w-[286px] h-[163px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'> For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p></div>
 
     <div><Link to={'/'} className='text-[20px] font-normal font-main text-[#B9FF66]' >Learn more</Link></div>
 
 </div>
 
+
+<div className='w-[286px] h-[163px]'>
+    <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'>For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.</p></div>
+
+    <div><Link to={'/'} className='text-[20px] font-normal font-main text-[#B9FF66]' >Learn more</Link></div>
+
+</div>
+
+<div className='w-[286px] h-[163px]'>
+    <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'> For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p></div>
+
+    <div><Link to={'/'} className='text-[20px] font-normal font-main text-[#B9FF66]' >Learn more</Link></div>
+
+</div>
 
 
 
