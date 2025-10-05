b8fee5557664af235440017b0349427a1da4f278
Antor Web-Dev <antorremeni@gmail.com>
2025-10-05 10:06:46 +0600

    chnage

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -10,7 +10,17 @@ const Case = () => {
 
     <section id='case' className='pb-[140px]'>
         <div className="container">   
-            <CommonHead commonh2={'Case Studies'} commonp={'Explore Real-Life Examples of Our Proven Digital Marketing Success through Our Case Studies'} />        
+
+            <div className='flex gap-[40px]'>
+
+
+            <CommonHead commonh2={'Case Studies'} />  
+            
+            <div className='w-[580px]'>
+             <CommonHead commonp={'Explore Real-Life Examples of Our Proven Digital Marketing  Success through Our Case Studies'}  />   
+
+            </div>
+            </div>
             <div id='case-id' className='bg-[#191A23] rounded-[45px] mt-[80px] pt-[70px] px-[60px] pb-[93px] flex justify-between '>
 
 
@@ -19,7 +29,7 @@ const Case = () => {
 <div  className='w-[286px] h-[163px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'> For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p></div>
 
-    <div><Link to={'/'} className='text-[20px] font-normal font-main text-[#B9FF66]' >Learn more</Link></div>
+    <div><Link to={'/'} className='text-[20px] font-normal font-main mt-[20px] text-[#B9FF66]' >Learn more</Link></div>
 
 </div>
 
@@ -28,7 +38,7 @@ const Case = () => {
 <div className='w-[286px] h-[163px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'>For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.</p></div>
 
-    <div><Link to={'/'} className='text-[20px] font-normal font-main text-[#B9FF66]' >Learn more</Link></div>
+    <div><Link to={'/'} className='text-[20px] font-normal font-main mt-[20px] text-[#B9FF66]' >Learn more</Link></div>
 
 </div>
 
@@ -38,7 +48,7 @@ const Case = () => {
 <div className='w-[286px] h-[138px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'> For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.</p></div>
 
-    <div><Link to={'/'} className='text-[20px] font-normal font-main text-[#B9FF66]' >Learn more</Link></div>
+    <div><Link to={'/'} className='text-[20px] font-normal font-main mt-[20px] text-[#B9FF66]' >Learn more</Link></div>
 
 </div>
 
