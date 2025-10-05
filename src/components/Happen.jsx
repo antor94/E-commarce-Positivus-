03223622ddf1ff4707f92767a60aaf0f6a0a98d6
Antor Web-Dev <antorremeni@gmail.com>
2025-10-05 09:44:15 +0600

    chnage

--- src/components/Happen.jsx
+++ src/components/Happen.jsx
@@ -7,8 +7,8 @@ const Happen = () => {
     
     <section id='happen' className='py-[23px]'>
         <div className="container">
-            <div id='happen-row' className=' rounded-[45px] bg-[#F3F3F3] '>
-                <div className=' relative'>
+            <div id='happen-row' className=' rounded-[45px] bg-[#F3F3F3] flex justify-center gap-[186px] '>
+                <div className=''>
 
                 <div><h2 className='text-[30px] font-medium font-main text-[#000]'>Let’s make things happen</h2></div>
                 <div className='w-[500px] py-[26px]'><p className='text-[18px] font-normal font-main text-[#000]'>Contact us today to learn more about how our digital marketing services can help your business grow and succeed online.</p></div>
@@ -16,10 +16,10 @@ const Happen = () => {
                        <button className='w-[264px] py-[20px] px-[35px] bg-[#191A23] rounded-[14px] text-[20px] font-normal font-main text-[#fff]'>Book a consultation</button> 
                 </div>
 {/* -------------------- happen-img */}
-            </div>
 <div className=''><img className='w-[359px] ' src={happenlogo} alt="" /></div>
                
 
+            </div>
         </div>
     </section>
     
