1cedff606c90e3563b9fc0e54f38be6095fb4475
Antor Web-Dev <antorremeni@gmail.com>
2025-10-05 09:48:46 +0600

    chnage

---
+++ src/components/Case.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Case = () => {
+  return (
+    <>
+
+
+
+    <section id='case'>
+        <div className="container">           
+            <div id='case-id' className='bg-[#191A23]'>
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>  
+            </div> 
+        
+          </section>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Case
