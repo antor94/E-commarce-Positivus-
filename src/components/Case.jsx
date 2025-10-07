b67551078898603d428feeab0d0a2ce66bffa61a
Antor Web-Dev <antorremeni@gmail.com>
2025-10-07 20:00:10 +0600

    change

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import CommonHead from './common/CommonHead'
 import { Link } from 'react-router'
+import { MdArrowOutward } from "react-icons/md";
+
 
 const Case = () => {
   return (
@@ -29,7 +31,7 @@ const Case = () => {
 <div  className='w-[286px] h-[163px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'> For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p></div>
 
-    <div><Link to={'/'} className='text-[20px] font-normal font-main mt-[20px] text-[#B9FF66]' >Learn more</Link></div>
+    <div className='mt-[20px]'><Link to={'/'} className='text-[20px] font-normal font-main  flex gap-[15px] text-[#B9FF66]' >Learn more <MdArrowOutward /></Link></div>
 
 </div>
 
@@ -38,7 +40,7 @@ const Case = () => {
 <div className='w-[286px] h-[163px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'>For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.</p></div>
 
-    <div><Link to={'/'} className='text-[20px] font-normal font-main mt-[20px] text-[#B9FF66]' >Learn more</Link></div>
+    <div className='mt-[20px]'><Link to={'/'} className='text-[20px] font-normal font-main  flex gap-[15px] text-[#B9FF66]' >Learn more <MdArrowOutward /></Link></div>
 
 </div>
 
@@ -48,7 +50,7 @@ const Case = () => {
 <div className='w-[286px] h-[138px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'> For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.</p></div>
 
-    <div><Link to={'/'} className='text-[20px] font-normal font-main mt-[20px] text-[#B9FF66]' >Learn more</Link></div>
+    <div className='mt-[20px]'><Link to={'/'} className='text-[20px] font-normal font-main flex gap-[15px] text-[#B9FF66]' >Learn more <MdArrowOutward /></Link></div>
 
 </div>
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,8 +8,8 @@ import { MdArrowOutward } from "react-icons/md";
 const Services = () => {
   return (
     <>
-      <section id="services">
-        <div className="container">
+      <section id="services" className="">
+        <div className="container " >
           <div id="" className="flex gap-[40px] mb-[80px] items-center">
             <CommonHead commonh2={"Services"} />
             <div className="w-[667px]">
@@ -21,10 +21,7 @@ const Services = () => {
             </div>
           </div>
           <div>
-
-
-
-          <div id="ser-col-1" className="flex">
+          <div id="ser-col-1" className="flex justify-around">
             <div className="w-[600px] h-[310px] flex gap-[70px] items-center p-[50px] rounded-[45px] border-b-4 bg-[#F3F3F3]">
               {/* ------------- left-side */}
               <div className="">
@@ -36,7 +33,19 @@ const Services = () => {
               </div>
               {/* ----------------- right side  */}
               <div><img src={serviceImg} alt="service-img" /> </div>        
-                </div>
+              </div>
+             <div className="w-[600px] h-[310px] flex gap-[70px] items-center p-[50px] rounded-[45px] border-b-4 bg-[#F3F3F3]">
+              {/* ------------- left-side */}
+              <div className="">
+              <div className="w-[221px] mb-[93px]"><p className="text-[30px] font-medium font-main text-[#000] bg-main ">Search engine <br /> <span className="w-[100px]">optimization</span> </p></div>
+              <Link to={'/'} className="text-[20px] font-normal flex items-center gap-[15px] font-main text-black">
+                <div className="w-[41px] h-[41px] bg-[#191A23] rounded-full flex justify-center items-center"><MdArrowOutward className="text-main" /> </div>
+                Learn more
+              </Link>           
+              </div>
+              {/* ----------------- right side  */}
+              <div><img src={serviceImg} alt="service-img" /> </div>        
+              </div>
           </div>
           </div>
         </div>
