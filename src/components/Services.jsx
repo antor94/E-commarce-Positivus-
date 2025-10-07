ec71e2b1e0876ce6d46e8bd6453345d5c533eb96
Antor Web-Dev <antorremeni@gmail.com>
2025-10-07 19:52:22 +0600

    chnage

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -25,59 +25,20 @@ const Services = () => {
 
 
           <div id="ser-col-1" className="flex">
-
             <div className="w-[600px] h-[310px] flex gap-[70px] items-center p-[50px] rounded-[45px] border-b-4 bg-[#F3F3F3]">
-              
-
               {/* ------------- left-side */}
               <div className="">
               <div className="w-[221px] mb-[93px]"><p className="text-[30px] font-medium font-main text-[#000] bg-main ">Search engine <br /> <span className="w-[100px]">optimization</span> </p></div>
-
-
               <Link to={'/'} className="text-[20px] font-normal flex items-center gap-[15px] font-main text-black">
-
                 <div className="w-[41px] h-[41px] bg-[#191A23] rounded-full flex justify-center items-center"><MdArrowOutward className="text-main" /> </div>
                 Learn more
-
-
-
-              </Link>
-               
-
+              </Link>           
               </div>
-
-{/* ----------------- right side  */}
-              <div>
-                <img src={serviceImg} alt="service-img" />
-
-
-
-
-
+              {/* ----------------- right side  */}
+              <div><img src={serviceImg} alt="service-img" /> </div>        
                 </div>
-               
-               
-               
-               
-               
-                </div>
-
-
-
           </div>
-
-
-
-
-
-
-
-
-
-
-
           </div>
-
         </div>
       </section>
     </>
