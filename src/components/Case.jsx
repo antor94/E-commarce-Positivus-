b59751a0080d966487603ca82096352e8d1f399f
Antor Web-Dev <antorremeni@gmail.com>
2025-10-08 01:50:26 +0600

    chnage

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -31,7 +31,7 @@ const Case = () => {
 <div  className='w-[286px] h-[163px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'> For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p></div>
 
-    <div className='mt-[20px]'><Link to={'/'} className='text-[20px] font-normal font-main  flex gap-[15px] text-[#B9FF66]' >Learn more <MdArrowOutward /></Link></div>
+    <div className='mt-[20px]'><Link to={'/'} className='text-[20px] font-normal font-main  flex items-center  gap-[15px] text-[#B9FF66]' >Learn more <MdArrowOutward /></Link></div>
 
 </div>
 
@@ -40,7 +40,7 @@ const Case = () => {
 <div className='w-[286px] h-[163px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'>For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.</p></div>
 
-    <div className='mt-[20px]'><Link to={'/'} className='text-[20px] font-normal font-main  flex gap-[15px] text-[#B9FF66]' >Learn more <MdArrowOutward /></Link></div>
+    <div className='mt-[20px]'><Link to={'/'} className='text-[20px] font-normal font-main  flex  items-center gap-[15px] text-[#B9FF66]' >Learn more <MdArrowOutward /></Link></div>
 
 </div>
 
@@ -50,7 +50,7 @@ const Case = () => {
 <div className='w-[286px] h-[138px]'>
     <div className='w-full'><p className='text-[18px] font-normal font-main text-[#fff]'> For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.</p></div>
 
-    <div className='mt-[20px]'><Link to={'/'} className='text-[20px] font-normal font-main flex gap-[15px] text-[#B9FF66]' >Learn more <MdArrowOutward /></Link></div>
+    <div className='mt-[20px]'><Link to={'/'} className='text-[20px] font-normal font-main flex  items-center gap-[15px] text-[#B9FF66]' >Learn more <MdArrowOutward /></Link></div>
 
 </div>
 
